9b97d61c718b55d2089d2585e5cd6af40c3fb83a
eemiljka <eemiljka@metropolia.fi>
2023-11-01 11:19:09 +0200

    express example not working completely

--- express/src/index.js
+++ express/src/index.js
@@ -1,11 +1,32 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { getItems, getItemsById } from "./items.js";
+
 const hostname = "127.0.0.1";
 const app = express();
 const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use("/docs", express.static(path.join(__dirname, "docs")));
 
 app.get("/", (req, res) => {
   res.send("Welcome to my REST API!");
 });
+
+// routing example
+app.get("/kukkuu", (request, response) => {
+  const myResponse = { message: "No moro!" };
+  response.json(myResponse);
+});
+
+//example generic items api
+app.get("/api/items", getItems);
+app.get("/api/items/:id", getItemsById);
+app.put("/api/items");
+app.post("/api/items");
+app.delete("/api/items");
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
---
+++ express/src/items.js
@@ -0,0 +1,94 @@
+// mock items data
+const items = [
+  { id: 5, name: "porkkana" },
+  { id: 6, name: "omena" },
+  { id: 19, name: "appelsiini" },
+];
+
+const getItems = (req, res) => {
+  res.json(items);
+};
+
+const getItemsById = (req, res) => {
+  console.log("getItemsById", req.params);
+  const item = items.find((element) => element.id == id);
+  if (item) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(item));
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end('{ message: "Item not found." }');
+  }
+};
+
+const postItem = (req, res) => {
+  let body = [];
+  req
+    .on("error", (err) => {
+      console.error(err);
+    })
+    .on("data", (chunk) => {
+      body.push(chunk);
+    })
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
+      console.log("req body", body);
+      body = JSON.parse(body);
+      // check if body is "valid"
+      if (!body.name) {
+        res.writeHead(400, { "Content-Type": "application/json" });
+        res.end(`{"message": "Missing data."}`);
+        return;
+      }
+
+      // check id of the last item in items and add 1
+      const newId = items[items.length - 1].id + 1;
+      items.push({ id: newId, name: body.name });
+      res.writeHead(201, { "Content-Type": "application/json" });
+      res.end(`{"message": "New item added."}`);
+    });
+};
+
+const deleteItem = (res, id) => {
+  const index = items.findIndex((item) => item.id == id);
+
+  if (index !== -1) {
+    items.splice(index, 1);
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(`{"message": "Item with id ${id} deleted."}`);
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(`{"message": "Item not found."}`);
+  }
+};
+
+const putItem = (res, id, req) => {
+  const item = items.find((element) => element.id == id);
+  if (item) {
+    let body = [];
+
+    req
+      .on("error", (err) => {
+        console.error(err);
+      })
+      .on("data", (chunk) => {
+        body.push(chunk);
+      })
+      .on("end", () => {
+        body = Buffer.concat(body).toString();
+        console.log("req body", body);
+        body = JSON.parse(body);
+
+        item.name = body.name || item.name;
+
+        res.writeHead(200, { "Content-Type": "application(json" });
+        res.end(`{"message": "Item with id ${id} updated."}`);
+      });
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(`{"message": "Item not found."}`);
+  }
+};
+
+export { getItems, getItemsById, postItem, deleteItem, putItem };
