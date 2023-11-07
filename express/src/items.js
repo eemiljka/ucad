63ee7c3cf897fa4dcfdb432361c595b86b5a74ca
eemiljka <eemiljka@metropolia.fi>
2023-11-07 15:17:00 +0200

    initializing

--- express/src/items.js
+++ express/src/items.js
@@ -5,90 +5,51 @@ const items = [
   { id: 19, name: "appelsiini" },
 ];
 
+/**
+ * Get all items request handler
+ * Amount of  objects in response can be limited by using 'limit' query param
+ *
+ * @param {object} req - http request
+ * @param {object} res - http response
+ */
 const getItems = (req, res) => {
-  res.json(items);
+  const limit = req.query.limit;
+  // TODO: check that the param value is int before using
+  if (limit) {
+    res.json(items.slice(0, limit));
+  } else {
+    res.json(items);
+  }
 };
 
+/**
+ *
+ * @param {*} req
+ * @param {*} res
+ */
 const getItemsById = (req, res) => {
+  // if item with id exists send it, otherwise send 404
   console.log("getItemsById", req.params);
-  const item = items.find((element) => element.id == id);
+  const item = items.find((element) => element.id == req.params.id);
   if (item) {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(item));
+    res.json(item);
   } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end('{ message: "Item not found." }');
+    res.status(404);
+    res.json({ message: "Item not found." });
   }
 };
 
 const postItem = (req, res) => {
-  let body = [];
-  req
-    .on("error", (err) => {
-      console.error(err);
-    })
-    .on("data", (chunk) => {
-      body.push(chunk);
-    })
-    .on("end", () => {
-      body = Buffer.concat(body).toString();
-      console.log("req body", body);
-      body = JSON.parse(body);
-      // check if body is "valid"
-      if (!body.name) {
-        res.writeHead(400, { "Content-Type": "application/json" });
-        res.end(`{"message": "Missing data."}`);
-        return;
-      }
-
-      // check id of the last item in items and add 1
-      const newId = items[items.length - 1].id + 1;
-      items.push({ id: newId, name: body.name });
-      res.writeHead(201, { "Content-Type": "application/json" });
-      res.end(`{"message": "New item added."}`);
-    });
-};
-
-const deleteItem = (res, id) => {
-  const index = items.findIndex((item) => item.id == id);
-
-  if (index !== -1) {
-    items.splice(index, 1);
-
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(`{"message": "Item with id ${id} deleted."}`);
+  console.log("new item posted", req.body);
+  // TODO: check last weeks example for generating an id
+  if (req.body.name) {
+    items.push({ id: 0, name: req.body.name });
+    res.sendStatus(201);
   } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(`{"message": "Item not found."}`);
+    res.sendStatus(400);
   }
 };
 
-const putItem = (res, id, req) => {
-  const item = items.find((element) => element.id == id);
-  if (item) {
-    let body = [];
+// TODO: add deleteItem(), putItem() and routing for those in index.js
 
-    req
-      .on("error", (err) => {
-        console.error(err);
-      })
-      .on("data", (chunk) => {
-        body.push(chunk);
-      })
-      .on("end", () => {
-        body = Buffer.concat(body).toString();
-        console.log("req body", body);
-        body = JSON.parse(body);
-
-        item.name = body.name || item.name;
-
-        res.writeHead(200, { "Content-Type": "application(json" });
-        res.end(`{"message": "Item with id ${id} updated."}`);
-      });
-  } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(`{"message": "Item not found."}`);
-  }
-};
-
-export { getItems, getItemsById, postItem, deleteItem, putItem };
+export { getItems, getItemsById, postItem };
